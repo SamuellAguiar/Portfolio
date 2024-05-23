bc1468511adb6bfa53256f3a94407e846f44a4c5
SamuellNote <samuellcarlosaguiar@gmail.com>
2024-05-23 20:22:18 -0300

    Add one button to the footer

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import Button from '@mui/material/Button';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', height: '30px', paddingBottom:'10px' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', height: '30px', paddingBottom: '10px' }}>
             <p>© 2024 Samuell Aguiar. All rights reserved.</p>
+
+            <Button onClick={scrollToTop} style={{ position: 'fixed', bottom: '20px', right: '20px', color: '#000000', backgroundColor: '#7e7e7e' }}>
+                <ArrowUpwardIcon />
+            </Button>
         </div>
     )
 }
