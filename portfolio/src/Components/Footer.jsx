007a9ad1c59d10e56a95e7fd87a7409e0bf448e5
SamuellNote <samuellcarlosaguiar@gmail.com>
2024-05-23 22:20:14 -0300

    Update the footer Component

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import IconButton from '@mui/material/IconButton';
 
 const Footer = () => {
     const scrollToTop = () => {
@@ -11,8 +16,59 @@ const Footer = () => {
     };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', height: '30px', paddingBottom: '10px' }}>
-            <p>© 2024 Samuell Aguiar. All rights reserved.</p>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            color: 'white',
+            height: '50px',
+            paddingBottom: '10px'
+        }}>
+            <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="#"
+                sx={{
+                    mr: 40,
+                    display: { xs: 'none', md: 'flex' },
+                    fontFamily: 'Roboto Condensed',
+                    fontWeight: 400,
+                    letterSpacing: '.2rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                }}
+            >
+                Samuell Aguiar
+            </Typography>
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: '25px',
+            }}>
+                <Typography>
+                    +55 (31) 9 8208-9884
+                </Typography>
+            </div>
+            <div>
+                <IconButton color="inherit">
+                    <a href="https://github.com/SamuellAguiar/Portfolio" target='_blank' style={{ color: 'inherit' }}>
+                        <GitHubIcon />
+                    </a>
+                </IconButton>
+                <IconButton color="inherit">
+                    <a href="https://www.linkedin.com/in/samuell-aguiar/" target='_blank' style={{ color: 'inherit' }}>
+                        <LinkedInIcon />
+                    </a>
+                </IconButton>
+                <IconButton color="inherit">
+                    <a href="https://www.instagram.com/samuell.ag/" target='_blank' style={{ color: 'inherit' }}>
+                        <InstagramIcon />
+                    </a>
+                </IconButton>
+            </div>
 
             <Button onClick={scrollToTop} style={{ position: 'fixed', bottom: '20px', right: '20px', color: '#000000', backgroundColor: '#7e7e7e' }}>
                 <ArrowUpwardIcon />
