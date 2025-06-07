45853b9e2989e6c47c823e0af3f0c61ed3ed27ae
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-06-07 12:03:12 -0300

    feat(contato): atualiza componente Contact.jsx para melhorias e correções

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -5,24 +5,38 @@ import {
      LinkedIn,
      Email,
 } from "@mui/icons-material";
+import emailjs from 'emailjs-com';
+import React, { useRef } from 'react';
 
 function Contact() {
+     const form = useRef();
+
+     const sendEmail = (e) => {
+          e.preventDefault();
+          emailjs.sendForm(
+               'service_7v5ynpq', // Substitua pelo seu Service ID
+               'template_g9vi1wj', // Substitua pelo seu Template ID
+               form.current,
+               'JfEVFBFv1GoGP43bo' // Substitua pelo seu User ID (public key)
+          ).then((result) => {
+               alert("Mensagem enviada com sucesso!");
+               form.current.reset(); // Limpa o formulário após o envio
+          }, (error) => {
+               alert("Erro ao enviar mensagem.");
+          });
+     };
+
      return (
-          <section className="contact-section" id="contact">
+          <section className="contact-section" id="contact" >
                <div className="contact-container">
                     {/* Formulário */}
-                    <form className="contact-form">
+                    <form className="contact-form" ref={form} onSubmit={sendEmail}>
                          <input type="text" placeholder="Digite o seu nome" required />
                          <input type="email" placeholder="Insira o seu e-mail" required />
                          <input type="tel" placeholder="Deixe o seu número para contato" />
-                         <textarea placeholder="Como posso te ajudar?" required></textarea>
+                         <textarea name="message" placeholder="Como posso te ajudar?" required></textarea>
                          <button type="submit">Enviar</button>
-                         <div className="social-icons">
-                              <a href="https://www.linkedin.com/in/samuell-aguiar/"><LinkedIn /></a>
-                              <a href="https://github.com/SamuellAguiar"><GitHub /></a>
-                              <a href="https://www.instagram.com/samuell.ag/"><Instagram /></a>
-                              <a href="mailto:samuellcarlosaguiar@gmail.com"><Email /></a>
-                         </div>
+
                     </form>
 
                     {/* Texto e contato */}
@@ -33,6 +47,14 @@ function Contact() {
                          </p>
                          <p><strong>samuellcarlosaguiar@gmail.com</strong></p>
                          <p><strong>(31) 98208-9884</strong></p>
+                         <p>Ou me encontre nas redes sociais:</p>
+                         {/* Ícones de redes sociais */}
+                         <div className="social-icons">
+                              <a href="https://www.linkedin.com/in/samuell-aguiar/"><LinkedIn /></a>
+                              <a href="https://github.com/SamuellAguiar"><GitHub /></a>
+                              <a href="https://www.instagram.com/samuell.ag/"><Instagram /></a>
+                              <a href="mailto:samuellcarlosaguiar@gmail.com"><Email /></a>
+                         </div>
                     </div>
                </div>
           </section>
