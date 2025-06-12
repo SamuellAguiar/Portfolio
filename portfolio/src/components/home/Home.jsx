254d27814dfa3ef31c0d5cd006238f6d93e7f203
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-06-12 16:03:30 -0300

    feat(home): atualiza estilos e componentes da Home
    Atualiza os arquivos Home.css e Home.jsx para melhorias visuais e funcionais.

--- portfolio/src/components/home/Home.jsx
+++ portfolio/src/components/home/Home.jsx
@@ -6,114 +6,100 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import ChatIcon from '@mui/icons-material/Chat';
 import React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function Home() {
+     const isMobile = useMediaQuery('(max-width:1024px)');
+
      return (
-          <>
-               <section className="about-section" id="home">
-                    <div className="about-text">
-                         <h1>
-                              Olá, Eu sou o<strong> Samuell</strong>.
-                         </h1>
-                         
-                         <h2>
-                              FullStack <span className="outline">Developer</span>
-                         </h2>
-                         
-                         <p>
-                              Sou um desenvolvedor apaixonado por criar experiências digitais. Tenho foco em
-                              interfaces modernas e responsivas, com atenção aos detalhes e usabilidade.
-                         </p>
-                         <div>
-                              <Stack direction="row" spacing={2} sx={{ marginTop: '1.5rem' }}>
-                                   <Button
-                                        variant="outlined"
-                                        startIcon={<GitHubIcon />}
-                                        href="https://github.com/SamuellAguiar"
-                                        target="_blank"
-                                        sx={{
-                                             color: '#000000',
-                                             borderColor: '#000000',
-                                             transition: 'background 0.2s, color 0.2s',
-                                             '&:hover': {
-                                                  backgroundColor: '#000000',
-                                                  color: '#fff',
-                                                  borderColor: '#000000',
-                                             },
-                                        }}
-                                   >
-                                        GitHub
-                                   </Button>
+          <section className="about-section" id="home">
+               <div className="about-text">
+                    <h1>
+                         Olá, Eu sou o<strong> Samuell</strong>.
+                    </h1>
 
-                                   <Button
-                                        variant="outlined"
-                                        startIcon={<InstagramIcon />}
-                                        href="https://www.instagram.com/samuell.ag/"
-                                        target="_blank"
-                                        sx={{
-                                             color: '#000000',
-                                             borderColor: '#000000',
-                                             transition: 'background 0.2s, color 0.2s',
-                                             '&:hover': {
-                                                  backgroundColor: '#000000',
-                                                  color: '#fff',
-                                                  borderColor: '#000000',
-                                             },
-                                        }}
-                                   >
-                                        Instagram
-                                   </Button>
+                    <h2>
+                         FullStack <span className="outline">Developer</span>
+                    </h2>
 
-                                   <Button
-                                        variant="outlined"
-                                        startIcon={<LinkedInIcon />}
-                                        href="https://www.linkedin.com/in/samuell-aguiar/"
-                                        target="_blank"
-                                        sx={{
-                                             color: '#000000',
-                                             borderColor: '#000000',
-                                             transition: 'background 0.2s, color 0.2s',
-                                             '&:hover': {
-                                                  backgroundColor: '#000000',
-                                                  color: '#fff',
-                                                  borderColor: '#000000',
-                                             },
-                                        }}
-                                   >
-                                        LinkedIn
-                                   </Button>
+                    <p>
+                         Sou um desenvolvedor apaixonado por criar experiências digitais. Tenho foco em
+                         interfaces modernas e responsivas, com atenção aos detalhes e usabilidade.
+                    </p>
 
-                                   <Button
-                                        variant="outlined"
-                                        startIcon={<ChatIcon />}
-                                        href="#contact"
-                                        sx={{
-                                             color: '#000000',
-                                             borderColor: '#000000',
-                                             transition: 'background 0.2s, color 0.2s',
-                                             '&:hover': {
-                                                  backgroundColor: '#000000',
-                                                  color: '#fff',
-                                                  borderColor: '#000000',
-                                             },
-                                        }}
-                                   >
-                                        Contato
-                                   </Button>
-                              </Stack>
-                         </div>
+                    <Stack
+                         direction={isMobile ? 'column' : 'row'}
+                         spacing={2}
+                         sx={{
+                              marginTop: '1.5rem',
+                              alignItems: 'center',
+                         }}
+                    >
+                         <Button
+                              variant="outlined"
+                              startIcon={<GitHubIcon />}
+                              href="https://github.com/SamuellAguiar"
+                              target="_blank"
+                              sx={buttonStyle}
+                         >
+                              GitHub
+                         </Button>
+
+                         <Button
+                              variant="outlined"
+                              startIcon={<InstagramIcon />}
+                              href="https://www.instagram.com/samuell.ag/"
+                              target="_blank"
+                              sx={buttonStyle}
+                         >
+                              Instagram
+                         </Button>
+
+                         <Button
+                              variant="outlined"
+                              startIcon={<LinkedInIcon />}
+                              href="https://www.linkedin.com/in/samuell-aguiar/"
+                              target="_blank"
+                              sx={buttonStyle}
+                         >
+                              LinkedIn
+                         </Button>
+
+                         <Button
+                              variant="outlined"
+                              startIcon={<ChatIcon />}
+                              href="#contact"
+                              sx={buttonStyle}
+                         >
+                              Contato
+                         </Button>
+                    </Stack>
+               </div>
+
+               <div className="about-image">
+                    <div className="photo-placeholder">
+                         <img
+                              src="/Me.jpeg"
+                              alt="Sua Foto"
+                         />
                     </div>
-                    <div className="about-image">
-                         <div className="photo-placeholder">
-                              <img
-                                   src="/Me.jpeg" // substitua pelo caminho real
-                                   alt="Sua Foto"
-                              />
-                         </div>
-                    </div>
-               </section>
-          </>
+               </div>
+          </section>
      );
 }
+
+const buttonStyle = {
+     color: '#000000',
+     borderColor: '#000000',
+     transition: 'background 0.2s, color 0.2s',
+     width: '100%',
+     maxWidth: '250px',
+     justifyContent: 'center',
+     '&:hover': {
+          backgroundColor: '#000000',
+          color: '#fff',
+          borderColor: '#000000',
+     },
+};
 
 export default Home;
